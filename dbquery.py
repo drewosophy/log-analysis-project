30df254e767d8fd04846e67aff39caa66adba001
Andrew Ologunebi <andrewologunebi@gmail.com>
2020-03-19 00:31:31 +0000

    Updated for PEP8 style

--- dbquery.py
+++ dbquery.py
@@ -2,6 +2,7 @@
 
 import psycopg2
 
+
 class txt:
     CYAN = '\033[96m'
     RED = '\033[91m'
@@ -9,6 +10,7 @@ class txt:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
+
 
 def top_articles():
     db = psycopg2.connect(database="news")
@@ -23,12 +25,14 @@ def top_articles():
                 """
     c.execute(query)
     result = c.fetchall()
-    
+
     title = '*** TOP ARTICLES ***'
-    print('\n' + txt.CYAN + txt.BOLD + txt.UNDERLINE + title.center(55) + txt.END + '\n')
+    print('\n' + txt.CYAN + txt.BOLD + txt.UNDERLINE
+          + title.center(55) + txt.END + '\n')
     for r in result:
-        print (txt.BOLD + "  ", r[0], ":  " + txt.END, r[1],"views"  )
+        print(txt.BOLD + "  ", r[0], ":  " + txt.END, r[1], "views")
     db.close()
+
 
 def top_authors():
     db = psycopg2.connect(database="news")
@@ -36,25 +40,29 @@ def top_authors():
     query = """
                 select authors.name, count(path) as page_views
                 from log, articles, authors
-                where log.path = '/article/' || articles.slug and authors.id = articles.author
+                where log.path = '/article/' || articles.slug
+                and authors.id = articles.author
                 group by name
                 order by page_views desc;
                 """
     c.execute(query)
     result = c.fetchall()
-   
+
     title = '*** TOP AUTHORS ***'
-    print('\n' + txt.GREEN + txt.BOLD + txt.UNDERLINE + title.center(55) + txt.END + '\n')
+    print('\n' + txt.GREEN + txt.BOLD + txt.UNDERLINE
+          + title.center(55) + txt.END + '\n')
     for r in result:
-        print (txt.BOLD + "  ", r[0], ":  " + txt.END, r[1],"views"  )
+        print(txt.BOLD + "  ", r[0], ":  " + txt.END, r[1], "views")
     db.close()
+
 
 def error_log():
     db = psycopg2.connect(database="news")
     c = db.cursor()
     query = """
                 select TO_CHAR(bad_req.dt,'Mon DD, YYYY') as date,
-                    CONCAT(CAST (ROUND(bad_req.num::decimal*100/all_req.num,2) as VARCHAR)) as percent
+                    CONCAT(CAST (ROUND(bad_req.num::decimal*100/all_req.num,2)
+                    as VARCHAR)) as percent
                 from (select DATE(time) as dt, COUNT(*) as num
                 from log
                     where CAST(substring(status, 1,1) as NUMERIC) > 2
@@ -68,14 +76,14 @@ def error_log():
     result = c.fetchall()
 
     title = '*** HIGH ERROR DAYS ***'
-    print('\n' + txt.RED + txt.BOLD + txt.UNDERLINE + title.center(55) + txt.END + '\n')
+    print('\n' + txt.RED + txt.BOLD + txt.UNDERLINE
+          + title.center(55) + txt.END + '\n')
     for r in result:
-        print (txt.BOLD + "  ", r[0], ":  " + txt.END, r[1],"%"  )
+        print(txt.BOLD + "  ", r[0], ":  " + txt.END, r[1], "%")
     db.close()
+
 
 if __name__ == '__main__':
     top_articles()
     top_authors()
     error_log()
-
-
